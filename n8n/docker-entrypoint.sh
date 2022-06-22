065b6332759a462f23ac6147e3e4ac62f5cc04c9
Niefuend, Florian <florian.niefuend@cewe.de>
2022-06-22 11:52:30 +0200

    add n8n (L0rdShrek/HA-addons#1)

---
+++ n8n/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+CONFIG_PATH="/data/options.json"
+N8N_PATH="/data/n8n"
+
+mkdir -p "${N8N_PATH}/.n8n"
+
+
+#####################
+## USER PARAMETERS ##
+#####################
+
+# REQUIRED
+
+
+export N8N_BASIC_AUTH_ACTIVE="$(jq --raw-output '.auth // empty' $CONFIG_PATH)"
+export N8N_BASIC_AUTH_USER="$(jq --raw-output '.auth_username // empty' $CONFIG_PATH)"
+export N8N_BASIC_AUTH_PASSWORD="$(jq --raw-output '.auth_password // empty' $CONFIG_PATH)"
+export GENERIC_TIMEZONE="$(jq --raw-output '.timezone // empty' $CONFIG_PATH)"
+export WEBHOOK_URL="$(jq --raw-output '.webhook_url // empty' $CONFIG_PATH)"
+export N8N_METRICS="$(jq --raw-output '.prometheus_metrics // empty' $CONFIG_PATH)"
+
+export NODE_FUNCTION_ALLOW_EXTERNAL="$(jq --raw-output '.allow_external // empty' $CONFIG_PATH)"
+export NODE_FUNCTION_ALLOW_BUILTIN="$(jq --raw-output '.allow_builtin // empty' $CONFIG_PATH)"
+
+export N8N_PROTOCOL="$(jq --raw-output '.protocol // empty' $CONFIG_PATH)"
+export N8N_SSL_CERT="/ssl/$(jq --raw-output '.certfile // empty' $CONFIG_PATH)"
+export N8N_SSL_KEY="/ssl/$(jq --raw-output '.keyfile // empty' $CONFIG_PATH)"
+export N8N_USER_FOLDER="${N8N_PATH}"
+
+if [ -z "${N8N_BASIC_AUTH_USER}" ] || [ -z "${N8N_BASIC_AUTH_ACTIVE}" ]; then
+    export N8N_BASIC_AUTH_ACTIVE=false
+    unset N8N_BASIC_AUTH_USER
+    unset N8N_BASIC_AUTH_PASSWORD
+fi
+
+
+###########
+## MAIN  ##
+###########
+
+
+if [ -d ${N8N_PATH} ] ; then
+  chmod o+rx ${N8N_PATH}
+  chown -R node ${N8N_PATH}/.n8n
+  ln -s ${N8N_PATH}/.n8n /home/node/
+fi
+
+chown -R node /home/node
+
+if [ "$#" -gt 0 ]; then
+  # Got started with arguments
+  exec su-exec node "$@"
+else
+  # Got started without arguments
+  exec su-exec node n8n
+fi
