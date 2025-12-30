8ad536ff23caa94d69f45adf66cead50ea2761d1
luksi1234 <luksi1234@gmail.com>
2025-12-30 14:39:15 +0000

    bugfix

--- doorbell/app/beepnoise2.py
+++ doorbell/app/beepnoise2.py
@@ -1,35 +1,5 @@
 import numpy as np
 
-# Parameters
-samplerate = 48000   # Hz
-frequency = 880      # Hz
-tone_duration = 0.25 # seconds
-silence_duration = 0.25
-
-
-class BeepNoise2(object):
-
-    def __init__(self, freq :int=880, duration :int=250, samplerate: int=16000 ):
-        self._freq = freq
-        self._duration = duration/1000
-        self._sample_rate = samplerate
-        #self._sample_rate = 48000
-        #self._volume = 1.0
-        #self._audio = []
-
-    def beep(self):
-        # Time axes
-        t_tone = np.linspace(0, self._duration, int(self._sample_rate * self._duration), False)
-        t_silence = np.zeros(int(self._sample_rate * self._duration))
-
-        # Generate sine wave
-        tone = 0.5 * np.sin(2 * np.pi * self._freq * t_tone)
-
-        # Concatenate tone + silence
-        signal = np.concatenate([tone, t_silence])
-
-        return signal
-
 
 def generate_sine_with_silence(
     frequency=880,
